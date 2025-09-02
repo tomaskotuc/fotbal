2189908b65c1984675e00d6c142b8a84fb36caae
tomaskotuc <tomask1298@gmail.com>
2025-09-02 14:19:25 +0200

    first commit

---
+++ app/Views/index.php
@@ -0,0 +1,5 @@
+<?= $this->extend('layout/template'); ?>
+<?= $this->section('content'); ?>
+<p class="text-center">Jsem černej</p>
+
+<?= $this->endSection(); ?>
---
+++ app/Views/layout/navbar.php
@@ -0,0 +1,7 @@
+<div class="bg-dark pt-2 pb-2">
+<h1 class="text-center text-white">Začínáme</h1>
+<a href="<?= base_url("/"); ?>" class="text-center text-white nav-link">Hlavní Stránka</a>
+
+
+</div>
+ 
